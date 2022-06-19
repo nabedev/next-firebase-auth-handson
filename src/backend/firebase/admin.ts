d3139d9532e2ad2e712abb1922410116ea005edb
nabedev <26590545+nabedev@users.noreply.github.com>
2022-06-20 00:00:17 +0900

    Use .env instead of json for admin credentials

--- src/backend/firebase/admin.ts
+++ src/backend/firebase/admin.ts
@@ -2,12 +2,14 @@ import * as admin from 'firebase-admin'
 import { applicationDefault } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
-// console.log(admin.apps.length)
-
 const getAdmin = (): admin.app.App => {
   if (admin.apps.length === 0) {
     return admin.initializeApp({
-      credential: applicationDefault(),
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
     })
   }
 
