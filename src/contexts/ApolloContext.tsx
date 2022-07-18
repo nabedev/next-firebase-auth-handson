7c34077f66d889405f5051c14fc8f18e89ce2d97
nabedev <26590545+nabedev@users.noreply.github.com>
2022-07-18 21:58:40 +0900

    Remove unused code

--- src/contexts/ApolloContext.tsx
+++ src/contexts/ApolloContext.tsx
@@ -8,9 +8,7 @@ const httpLink = createHttpLink({
 })
 
 const authLink = setContext(async (_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = await auth.currentUser?.getIdToken()
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
